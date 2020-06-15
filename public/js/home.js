dcf50e9788d84730f4875e5aa45f5de8663819ed
IRLANDEC0151 <37473451+IRLANDEC0151@users.noreply.github.com>
2020-06-15 04:25:35 +0300

    исправлены баги  в добавлении соц сети

--- public/js/home.js
+++ public/js/home.js
@@ -16,22 +16,22 @@ $(document).ready(function () {
 //добавление input для социальной сети
 
 let addSocialBtn = document.querySelector(".addbook-addSocialWebBtn");
-
+let phoneInput = document.getElementById("beforeSocial");
 addSocialBtn.addEventListener("click", addSocialLink);
 let maxLink = 0;
+let socialLinkArr = [
+  "https://vk.com/irlanndec",
+  "https://www.instagram.com/punkybrain/",
+  "https://vk.com/id527353857",
+];
 function addSocialLink() {
-  if (maxLink === 3) {
+  let countSpan = phoneInput.getElementsByTagName("span").length;
+  if (countSpan === 3) {
     addSocialBtn.textContent = "Информации достаточно";
     return 0;
   }
   maxLink++;
-  console.log(maxLink);
-  let socialLinkArr = [
-    "https://vk.com/irlanndec",
-    "https://www.instagram.com/punkybrain/",
-    "https://vk.com/id527353857"
-  ];
-  document.getElementById("beforeSocial").insertAdjacentHTML(
+  phoneInput.insertAdjacentHTML(
     "beforeend",
     `
     <div class="form-group" id="socialLinkInput${maxLink}"> 
@@ -40,7 +40,7 @@ function addSocialLink() {
       type="text"
       class="form-control"
       id="exampleInputPassword1"
-      placeholder=" Пример: ${socialLinkArr[maxLink-1]}"
+      placeholder=" Пример: ${socialLinkArr[countSpan]}"
       style="font-size: 1.5rem"
       required
     />
@@ -52,7 +52,6 @@ function addSocialLink() {
   ) {
     if (event.target.tagName == "IMG") {
       this.remove();
-      maxLink--;
     }
     addSocialBtn.innerHTML = ` <img src="./add.svg" width="15px"  />Указать
       социальную сеть`;
